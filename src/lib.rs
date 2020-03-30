f232528e1496052734d962bcd597871642962734
Ricardo Rodríguez Lorenzo <rrodriguez86@gmail.com>
2020-03-30 23:48:44 +0100

    Add Hardware Interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 pub fn test_runner(tests: &[&dyn Fn()]) {
@@ -45,7 +47,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 /// Entry point for `cargo xtest`
@@ -54,11 +56,17 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info);
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -54,8 +54,7 @@ pub extern "C" fn _start() -> ! {
     //panic!("Some panic message");
 
     println!("It did not crash!");
-
-    loop{}
+    blog_os::hlt_loop();
 }
 
 // This function is called on panic.
@@ -63,7 +62,7 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    blog_os::hlt_loop();
 }
 
 // Panic handler in test mode.
