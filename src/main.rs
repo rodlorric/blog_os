085bde4e26ee9917bf2564e72ba615ff8be8691c
Ricardo Rodríguez Lorenzo <rrodriguez86@gmail.com>
2020-04-05 01:10:13 +0100

    Add Introduction to paging

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,11 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
     // invoke a breakpoint exception
     //x86_64::instructions::interrupts::int3();
 
@@ -41,9 +46,20 @@ pub extern "C" fn _start() -> ! {
     //    *(0xdeadbeef as *mut u64) = 42;
     //};
 
-    fn stack_overflow() {
-        stack_overflow(); // for each recursion, the return address is pushed
-    }
+    //let ptr = 0xdeadbeaf as *mut u32;
+    let ptr = 0x203192 as *mut u32;
+
+    //read from a code page
+    unsafe { let x = *ptr; }
+    println!("read worked");
+
+    // write to a code page
+    unsafe { *ptr = 42; }
+    println!("write worked");
+
+    //fn stack_overflow() {
+    //    stack_overflow(); // for each recursion, the return address is pushed
+    //}
 
     // trigger a stack overflow
     //stack_overflow();
